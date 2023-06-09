9997d747d5a483128a755a090a4649c23fabeaba
Faruk Ahmed <farukahmed997.fa@gmail.com>
2023-06-09 23:30:01 +0530

    removed unused dependencies

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
-import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
 
 export const TodoWrapper = () => {
